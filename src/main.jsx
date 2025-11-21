b300c01d91524f4d24b28c95908e5a3356a9a1b3
MatiasLago5 <matiaslagoscarro@gmail.com>
2025-11-21 12:58:46 -0300

    Actializando repositorio de frontend

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import App from './App.jsx';
-import { AuthProvider } from './context/AuthContext.jsx';
+import { AuthProvider } from './context/AuthProvider.jsx';
+import ScrollToTop from './components/ScrollToTop.jsx';
 import { store } from './store';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
+        <ScrollToTop />
         <AuthProvider>
           <App />
         </AuthProvider>
